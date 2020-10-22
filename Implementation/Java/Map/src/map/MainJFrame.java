092080ad9335ff0ce2fdf07d11c668eaf2e05a38
bao1305946@gmail.com <bao1305946@gmail.com>
2020-10-22 14:46:31 +0700

    update

--- Implementation/Java/Map/src/map/MainJFrame.java
+++ Implementation/Java/Map/src/map/MainJFrame.java
@@ -5,13 +5,40 @@
  */
 package map;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
+import java.awt.Shape;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.geom.Rectangle2D;
+import java.io.File;
 import java.util.Random;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.Border;
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortDataListener;
+import com.fazecast.jSerialComm.SerialPortEvent;
+import javax.swing.JTextArea;
 
 /**
  *
@@ -24,58 +51,178 @@ public class MainJFrame extends javax.swing.JFrame {
      */
     int width;
     int height;
+    int width_screen;
+    int heigh_screen;
     int x;
     int y;
     int x_old;
     int y_old;
+    private Point points[] = new Point[2];
+    int width_screen_map;
+    int heigh_screen_map;
+
+    private JLabel jLabelTitle;
+    private JLabel jLabel_A;
+    private JLabel jLabel_B;
+    private JLabel jLabel_C;
+    private JLabel jLabel_D;
+
+    private JButton jbtConnect;
+    private JButton jbtCheckRF;
+    private JButton jbtExecute;
+    private JTextArea  jtaInFo;
+
+    void setGuiTemp() {
+        jLabelTitle = new JLabel("Chương Trình Giám Sát Đuối Nước");
+        jLabelTitle.setBounds(heigh_screen_map + 250, 40, 700, 50);
+        //jLabelTitle.setBorder(new javax.swing.border.LineBorder(Color.RED, 1, true));
+        jLabelTitle.setFont(new Font("Times New Roman", Font.BOLD, 34));
+        jLabelTitle.setHorizontalAlignment(SwingConstants.CENTER);
+        jLabelTitle.setVerticalAlignment(SwingConstants.CENTER);
+
+        jLabel_A = new JLabel("A");
+        jLabel_B = new JLabel("B");
+        jLabel_C = new JLabel("C");
+        jLabel_D = new JLabel("D");
+
+        jLabel_A.setFont(new Font("Times New Roman", Font.BOLD, 34));
+        jLabel_B.setFont(new Font("Times New Roman", Font.BOLD, 34));
+        jLabel_C.setFont(new Font("Times New Roman", Font.BOLD, 34));
+        jLabel_D.setFont(new Font("Times New Roman", Font.BOLD, 34));
+
+        jLabel_D.setBounds(10, 25, 50, 50);
+        jLabel_C.setBounds(width_screen_map + 60, 25, 50, 50);
+        jLabel_B.setBounds(width_screen_map + 60, heigh_screen_map, 50, 50);
+        jLabel_A.setBounds(10, heigh_screen_map, 50, 50);
+
+        int X_jLabelPort = width_screen_map + 100;
+        int Y_jLabelPort = 150;
+        jLabelPort = new JLabel("Port");
+        jLabelPort.setBounds(X_jLabelPort, Y_jLabelPort, 100, 50);
+        jLabelPort.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabelPort.setHorizontalAlignment(SwingConstants.LEFT);
+
+        jComboBoxPort = new JComboBox<String>();
+        jComboBoxPort.setBounds(X_jLabelPort + 120, Y_jLabelPort + 10, 100, 30);;
+        jComboBoxPort.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N       
+
+        for (SerialPort com : SerialPort.getCommPorts()) {
+            String s = com.getSystemPortName();
+            jComboBoxPort.addItem(s);
+        }
+
+        JLabel jLabelPort1 = new JLabel("Connect");
+        jLabelPort1.setBounds(X_jLabelPort, Y_jLabelPort + 50, 100, 50);
+        jLabelPort1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabelPort1.setHorizontalAlignment(SwingConstants.LEFT);
+
+        jbtConnect = new JButton("Open");
+        jbtConnect.setBounds(X_jLabelPort + 120, Y_jLabelPort + 60, 100, 30);
+        jbtConnect.setFont(new Font("Times New Roman", Font.BOLD, 18));
+
+        JLabel jLabelPort2 = new JLabel("Check RF");
+        jLabelPort2.setBounds(X_jLabelPort, Y_jLabelPort + 50 + 50, 100, 50);
+        jLabelPort2.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabelPort2.setHorizontalAlignment(SwingConstants.LEFT);
+
+        jbtCheckRF = new JButton("Test");
+        jbtCheckRF.setBounds(X_jLabelPort + 120, Y_jLabelPort + 60 + 50, 100, 30);
+        jbtCheckRF.setFont(new Font("Times New Roman", Font.BOLD, 18));
+
+                       
+        JLabel jLabelPort3 = new JLabel("Execute");
+        jLabelPort3.setBounds(X_jLabelPort, Y_jLabelPort + 50 + 50 + 50, 100, 50);
+        jLabelPort3.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabelPort3.setHorizontalAlignment(SwingConstants.LEFT);
+
+        jbtExecute = new JButton("Run");
+        jbtExecute.setBounds(X_jLabelPort + 120, Y_jLabelPort + 60 + 50 + 50, 100, 30);
+        jbtExecute.setFont(new Font("Times New Roman", Font.BOLD, 18));
+        
+        JLabel jLabelPort4 = new JLabel("Information");
+        jLabelPort4.setBounds(X_jLabelPort, Y_jLabelPort + 50 + 50 + 50 + 50, 130, 50);
+        jLabelPort4.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        jLabelPort4.setHorizontalAlignment(SwingConstants.LEFT);
+        
+        jtaInFo = new JTextArea("The system have not started !!!");
+        jtaInFo.setBounds(X_jLabelPort, Y_jLabelPort + 50 + 50 + 50 + 50 + 40, width_screen - width_screen_map - 150, 300);
+        jtaInFo.setFont(new java.awt.Font("Tahoma", 1, 16)); // NOI18N
+        jtaInFo.setBorder(new javax.swing.border.LineBorder(Color.BLACK, 1, true));
+        
+        add(jLabel_A);
+        add(jLabel_B);
+        add(jLabel_C);
+        add(jLabel_D);
+        add(jLabelTitle);
+        add(jLabelPort1);
+        add(jLabelPort2);
+        add(jLabelPort3);
+        add(jLabelPort4);
+        add(jtaInFo);
+        add(jbtConnect);
+        add(jbtCheckRF);
+        add(jbtExecute);
+        add(jComboBoxPort);
+        add(jLabelPort);
+
+        onClickConnect();
+        onClickCheck();
+    }
+
+    private void onClickConnect() {
+        jbtConnect.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               
+            }
+        });
+    }
+
+    private void onClickCheck() {
+        jbtCheckRF.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
     public MainJFrame() {
-        Random rand = new Random();
+
+        heigh_screen = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+        width_screen = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
+        System.out.println("width_screen: " + width_screen);
+        System.out.println("heigh_screen: " + heigh_screen);
 
         initComponents();
-        jPanelMap.setBackground(Color.green);
-        getContentPane().setBackground( Color.WHITE );
-        height = jPanelMap.getHeight();
-        width = jPanelMap.getWidth();
-        System.out.println("Heigh: " + height);
-        System.out.println("Width: " + width);
+        getContentPane().setBackground(Color.WHITE);
+        width_screen_map = heigh_screen;
+        heigh_screen_map = heigh_screen - 150;
+        setGuiTemp();
+
+        System.out.println("width_screen_map: " + width_screen_map);
+        System.out.println("heigh_screen_map: " + heigh_screen_map);
+        PaintPanel paintPanel = new PaintPanel(20, 30);
+        paintPanel.setBounds(50, 40, width_screen_map, heigh_screen_map);
+        paintPanel.setBackground(Color.WHITE);
+        paintPanel.setBorder(new javax.swing.border.LineBorder(Color.RED, 2, true));
+        add(paintPanel);
+
+        Random rand = new Random(100);
         ScheduledExecutorService service__ = Executors.newSingleThreadScheduledExecutor();
         service__.scheduleAtFixedRate(new Runnable() {
             @Override
             public void run() {
-                int xx = rand.nextInt(width-20);
-                int yy = rand.nextInt(height-20);
-                
-                x = xx; 
+                int xx = rand.nextInt(200);
+                int yy = rand.nextInt(200);
+
+                x = xx;
                 y = yy;
+                paintPanel.setX_point(x);
+                paintPanel.setY_point(y);
                 repaint();
             }
         }, 0, 1000, TimeUnit.MILLISECONDS);
-
-    }
-
-    public void paint(Graphics g) {
-        g.clearRect(x_old, y_old, 10, 10);
-        g.clearRect(x_old+20, y_old, 10, 10);
-        g.clearRect(x_old+40, y_old, 10, 10);
-        int i;
-        int line = 30;
-        int rowHt = height / line;
-        for (i = 0; i < line; i++) {
-            g.drawLine(0, i * rowHt, width, i * rowHt);
-        }
-
-        // draw the columns
-        int rowWid = width / line;
-        for (i = 0; i < line; i++) {
-            g.drawLine(i * rowWid, 0, i * rowWid, height);
-        }
-        
-        g.fillOval(x, y, 10, 10);
-        g.fillOval(x+20, y, 10, 10);
-        g.fillOval(x+40, y, 10, 10);
-        x_old = x;
-        y_old = y;
-        g.setColor(Color.red);
     }
 
     /**
@@ -87,40 +234,18 @@ public class MainJFrame extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jPanelMap = new javax.swing.JPanel();
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(254, 254, 254));
-
-        jPanelMap.setBackground(new java.awt.Color(254, 254, 254));
-        jPanelMap.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
-
-        javax.swing.GroupLayout jPanelMapLayout = new javax.swing.GroupLayout(jPanelMap);
-        jPanelMap.setLayout(jPanelMapLayout);
-        jPanelMapLayout.setHorizontalGroup(
-            jPanelMapLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1076, Short.MAX_VALUE)
-        );
-        jPanelMapLayout.setVerticalGroup(
-            jPanelMapLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 589, Short.MAX_VALUE)
-        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanelMap, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+            .addGap(0, 1567, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jPanelMap, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+            .addGap(0, 673, Short.MAX_VALUE)
         );
 
         pack();
@@ -162,6 +287,179 @@ public class MainJFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JPanel jPanelMap;
     // End of variables declaration//GEN-END:variables
+    public class PaintPanel extends JPanel {
+
+        private int x_point;
+        private int y_point;
+
+        public int getX_point() {
+            return x_point;
+        }
+
+        public void setX_point(int x_point) {
+            this.x_point = x_point;
+        }
+
+        public int getY_point() {
+            return y_point;
+        }
+
+        public void setY_point(int y_point) {
+            this.y_point = y_point;
+        }
+
+        public PaintPanel(int x, int y) {
+            super();
+            x_point = x;
+            y_point = y;
+            this.setBackground(Color.LIGHT_GRAY);
+            points[0] = new Point(x_point, y_point);
+            repaint();
+        }
+
+        public void paint(Graphics g) {
+            super.paint(g); // clears drawing area
+            g.fillOval(x_point, y_point, 10, 10);
+            g.fillOval(x_point + 20, y_point, 10, 10);
+            g.fillOval(x_point + 40, y_point, 10, 10);
+
+            g.setColor(Color.red);
+            g.setFont(new Font("Courier", Font.BOLD, 13));
+            int line = 10;
+
+            int rowHt = (width_screen_map) / line;
+            //g.drawLine(rowHt - 50, 30, rowHt - 50, heigh_screen_map - 30);
+            for (int i = 0; i < line; i++) {
+                g.drawLine(i * rowHt, 0, i * rowHt, heigh_screen_map);
+                if (i == 0) {
+                    g.drawString(String.valueOf(i), i * rowHt + 3, heigh_screen_map - 5);
+                } else if (i < 10) {
+                    g.drawString(String.valueOf(i), i * rowHt - 10, heigh_screen_map - 5);
+                } else if (i >= 10) {
+                    g.drawString(String.valueOf(i), i * rowHt - 20, heigh_screen_map - 5);
+                }
+            }
+            g.drawString(String.valueOf(line), line * rowHt - 20, heigh_screen_map - 5);
+//
+            int rowWid = heigh_screen_map / line;
+            for (int i = 0; i < line; i++) {
+                g.drawLine(0, i * rowWid, width_screen_map, i * rowWid);
+                if (i != 0) {
+                    g.drawString(String.valueOf(line - i), 3, i * rowWid + 12);
+                }
+            }
+            g.drawString(String.valueOf(line), 3, 12);
+        }
+    }
+
+    // Configuation Fram
+    private JLabel jLabelPort;
+    private JComboBox<String> jComboBoxPort;
+    private JLabel jLabelName;
+    private JTextField jTextFieldPathFile;
+    private JButton jButtonNameFile;
+
+    private JButton jButtonOK;
+    private JButton jButtonCancel;
+    private JButton jButtonApply;
+
+    private int iIndexPort = 0;
+
+    private void createFramSetup() {
+        boolean isStatusApply = false;
+        JFrame f = new JFrame("Configuration");
+        f.getContentPane().setBackground(Color.green);
+        f.setSize(750, 250);
+        f.setLocation(width_screen_map + 100, 300);
+        //Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
+
+        jLabelPort = new JLabel("Port Name");
+        jLabelPort.setLocation(5, 5);
+        jLabelPort.setSize(100, 30);
+        jLabelPort.setFont(new java.awt.Font("Tahoma", 1, 16)); // NOI18N
+
+        jComboBoxPort = new JComboBox<String>();
+        jComboBoxPort.setLocation(105, 5);
+        jComboBoxPort.setSize(90, 30);
+        jComboBoxPort.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+
+        for (SerialPort com : SerialPort.getCommPorts()) {
+            String s = com.getSystemPortName();
+            jComboBoxPort.addItem(s);
+        }
+
+        jComboBoxPort.setSelectedItem(jComboBoxPort.getItemAt(iIndexPort));
+
+        jLabelName = new JLabel("Path File");
+        jLabelName.setLocation(5, 40);
+        jLabelName.setSize(100, 30);
+        jLabelName.setFont(new java.awt.Font("Tahoma", 1, 16)); // NOI18N
+
+        jTextFieldPathFile = new JTextField("");
+        jTextFieldPathFile.setLocation(105, 40);
+        jTextFieldPathFile.setSize(550, 30);
+        jTextFieldPathFile.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
+        jTextFieldPathFile.setEditable(false);
+
+        jButtonNameFile = new JButton("...");
+        jButtonNameFile.setLocation(660, 40);
+        jButtonNameFile.setSize(50, 30);
+        jButtonNameFile.setFont(new java.awt.Font("Tahoma", 1, 16)); // NOI18N
+        jButtonNameFile.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFileChooser c = new JFileChooser();
+
+                c.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+                c.showOpenDialog(null);
+                File file = c.getSelectedFile();
+                String fullPath = file.getAbsolutePath();
+                System.out.println("sPath: " + fullPath);
+            }
+        });
+
+        // Button OK
+        jButtonOK = new JButton("OK");
+        jButtonOK.setLocation(250 - 50, 140);
+        jButtonOK.setSize(100, 35);
+        jButtonOK.setFont(new java.awt.Font("Tahoma", 1, 16)); // NOI18N
+        jButtonOK.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!isStatusApply) {
+                    System.out.println("jButtonOK is called");
+                    iIndexPort = jComboBoxPort.getSelectedIndex();
+                    String comPortMain = SerialPort.getCommPort(jComboBoxPort.getItemAt(iIndexPort)).toString();
+                    System.out.println("comPortMain: " + comPortMain);
+                }
+                f.dispose();
+            }
+        });
+
+        // Button Cancel
+        jButtonCancel = new JButton("Cancel");
+        jButtonCancel.setLocation(750 / 2 + 75, 140);
+        jButtonCancel.setSize(100, 35);
+        jButtonCancel.setFont(new java.awt.Font("Tahoma", 1, 16)); // NOI18N
+        jButtonCancel.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("jButtonCancel is called");
+                f.dispose();
+
+            }
+        });
+
+        f.add(jLabelPort);
+        f.add(jComboBoxPort);
+        f.add(jLabelName);
+        f.add(jTextFieldPathFile);
+        f.add(jButtonNameFile);
+        f.add(jButtonOK);
+        f.add(jButtonCancel);
+        f.setLayout(null);
+        f.setVisible(true);
+    }
+
 }
--- Implementation/Java/Map/src/map/Map.java
+++ Implementation/Java/Map/src/map/Map.java
@@ -15,6 +15,11 @@ import javax.swing.JPanel;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseMotionAdapter;
+import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import javax.swing.border.Border;
 
 public class Map extends JApplet {
 
@@ -28,94 +33,88 @@ public class Map extends JApplet {
     private int r = 8;
     JLabel label1;
     JLabel label2;
-
+    int width_screen;
+    int heigh_screen;
     public void init() {
 
         JPanel p = new JPanel();
-
+        p.setVisible(true);
+        
+        heigh_screen = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
+        width_screen = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
         label1 = new JLabel("Click several times to create a polygon. Finish by clicking on Point 0.");
         label1.setBackground(Color.CYAN);
         label1.setOpaque(true);
         label1.setVisible(true);
         label1.setPreferredSize(new Dimension(1000, 400));
         label1.setSize(new Dimension(1000, 400));
+        label1.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 2, true));
         p.setLayout(new FlowLayout());
         p.add(label1);
         Container content = getContentPane();
         content.setLayout(new BorderLayout()); // Used to center the panel
         content.add(p, BorderLayout.SOUTH);
 
-        PaintPanel paintPanel = new PaintPanel();
+        PaintPanel paintPanel = new PaintPanel(30, 30);
+        paintPanel.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 2, true));
+        paintPanel.setBounds(5, 5, 500, 500);
         content.add(paintPanel, BorderLayout.CENTER);
+        repaint();
+
+        Random rand = new Random(100);
+        ScheduledExecutorService service__ = Executors.newSingleThreadScheduledExecutor();
+        service__.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                int xx = rand.nextInt(200);
+                int yy = rand.nextInt(200);
+                paintPanel.setX_point(xx);
+                paintPanel.setY_point(yy);
+                repaint();
+            }
+        }, 0, 1000, TimeUnit.MILLISECONDS);
 
     }
 
     public class PaintPanel extends JPanel {
 
-        // set up GUI and register mouse event handler
-        public PaintPanel() {
+        private int x_point;
+        private int y_point;
+
+        public void setX_point(int x_point) {
+            this.x_point = x_point;
+        }
+
+        public void setY_point(int y_point) {
+            this.y_point = y_point;
+        }
+
+        public PaintPanel(int x, int y) {
             super();
+            x_point = x;
+            y_point = y;
             this.setBackground(Color.LIGHT_GRAY);
-
-            // handle frame mouse  event
-            addMouseListener(
-                    new MouseAdapter() // anonymous inner class
-            {
-                // define a point and repaint
-                public void mousePressed(MouseEvent event) {
-                    if (!p0Exists) {
-                        // create point0
-                        points[0] = new Point(event.getX(), event.getY());
-                        System.out.println("Point 0 created: " + points[0]);
-                        p0Exists = true;
-                        repaint();
-                    } else if (!p1Exists) {
-                        // create point1
-                        points[1] = new Point(event.getX(), event.getY());
-                        System.out.println("Point 1 created: " + points[1]);
-                        p1Exists = true;
-                        repaint();
-                    }
-
-                } // end method mousePressed
-
-            } // end anonymous inner class
-            ); // end call to addMouseMotionListener
-        } // end PaintPanel constructor
+            points[0] = new Point(x_point, y_point);
+            repaint();
+        }
 
         public void paint(Graphics g) {
             super.paint(g); // clears drawing area
-            if (p0Exists) {
-                g.drawOval(points[0].x - r / 2, points[0].y - r / 2, r, r);
-                g.drawString("Point0", points[0].x, points[0].y - 3 * r);
-                g.drawString("x=" + points[0].x + " y=" + points[0].y,
-                        points[0].x, points[0].y - r);
-                //g.drawLine(points[ 0 ].x - r/2, points[ 0 ].y - r/2, r, r);
-            }
-            if (p1Exists) {
-                g.drawOval(points[1].x - r / 2, points[1].y - r / 2, r, r);
-                g.drawString("Point1", points[1].x, points[1].y - 3 * r);
-                g.drawString("x=" + points[1].x + " y=" + points[1].y,
-                        points[1].x, points[1].y - r);
-            }
-            if (p0Exists && p1Exists) {
-                g.drawLine(points[0].x, points[0].y, points[1].x, points[1].y);
-            }
+            g.fillOval(x_point, y_point, 8, 8);
+
+            int line = 10;
+            int rowHt = 200 / line;
             
-            int rows = 20;
-            int cols = 20;
-            
-//            int rowHt = height / (rows);
-//            for (int i = 0; i < rows; i++) {
-//                g.drawLine(0, i * rowHt, width, i * rowHt);
-//            }
-//
-//            // draw the columns
-//            int rowWid = width / (cols);
-//            for (int i = 0; i < cols; i++) {
-//                g.drawLine(i * rowWid, 0, i * rowWid, height);
-//            }
-        } 
+            for (int i = 0; i < line; i++) {
+                g.drawLine(0, i * rowHt, 200, i * rowHt);
+            }
+
+            int rowWid = 200 / line;
+            for (int i = 0; i < line; i++) {
+                g.drawLine(i * rowWid, 0, i * rowWid, 200);
+            }
+
+        }
     }
 
 }
